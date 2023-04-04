13f9cf7ba18ae33a9780827c463b76573b5ab8cf
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-04-04 00:23:54 -0400

    added shapes test and inquirer function

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const inquirer = require('inquirer');
-const {circle, square, triangle} = require('./lib/shapes');
+const { Square, Circle, triangle } = require('./lib/shapes');
+const fs = require('fs');
 
 const questions = [
     {
-        type:'imput',
-        name:'text',
-        message:'Enter up to 3 characters'
-    }
+        type: 'imput',
+        name: 'text',
+        message: 'Enter up to 3 characters'
+    },
     {
-        type:'list',
-        name:'shape',
-        message:'Chose a shape',
-        Choices: ['circle', 'square', 'triangle']
-    }
+        type: 'list',
+        name: 'shape',
+        message: 'Chose a shape',
+        choices: ['circle', 'square', 'triangle']
+    },
     {
-        type:'imput',
-        name:'text-color',
-        message:'Enter color by name or number'
-    }
+        type: 'imput',
+        name: 'text-color',
+        message: 'Enter color by name or number'
+    },
     {
-        type:'imput',
-        name:'shape-color',
-        message:'Enter color by name or number'
+        type: 'imput',
+        name: 'shape-color',
+        message: 'Enter color by name or number'
     }
 ]
+
+function init() {
+    inquirer.prompt(questions)
+        .then(function (answer) {
+            console.log(answer);
+        });
+};
+
+init();
+
