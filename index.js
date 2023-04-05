5ca4a30e0dbf153dbc2e22b946ef1ff42be419f9
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-04-04 21:53:20 -0400

    added final functionality to app

--- index.js
+++ index.js
@@ -1,60 +1,84 @@
 const inquirer = require('inquirer');
-const { Square, Circle, triangle } = require('./lib/shapes');
+const { Color, Square, Circle, Triangle } = require('./lib/shapes');
 const fs = require('fs');
 
 const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Chose a shape',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
     {
         type: 'imput',
         name: 'text',
         message: 'Enter up to 3 characters'
     },
     {
-        type: 'list',
-        name: 'shape',
-        message: 'Chose a shape',
-        choices: ['circle', 'square', 'triangle']
-    },
-    {
         type: 'imput',
-        name: 'text-color',
+        name: 'textColor',
         message: 'Enter color by name or number for the text'
     },
     {
         type: 'imput',
-        name: 'shape-color',
+        name: 'shapeColor',
         message: 'Enter color by name or number for the shape'
     }
 ]
 
 class svg {
     constructor() {
-        this.textElement
         this.shapeElement
+        this.textElement
     };
     render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.textElement}${this.shapeElement}</svg>`
-    };
-    setTextElement(text, color) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">"${text}"</text>`
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     };
     setShapeElement(shape) {
         this.shapeElement = shape.render()
     };
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    };
 };
 
-// function to render svg based on user input
+
 function init() {
     inquirer.prompt(questions)
         .then(function (answer) {
             console.log(answer);
-        });
 
-    if (condition) {
-        
-    }
+
+            let newShape;
+
+            if (answer.shape === 'Square') {
+                newShape = new Square();
+            }
+            else if (answer.shape === 'Circle') {
+                newShape = new Circle();
+            }
+            else if (answer.shape === 'Triangle') {
+                newShape = new Triangle();
+            }
+
+            newShape.setColor(answer.shapeColor);
+
+            const newSVG = new svg();
+
+            newSVG.setShapeElement(newShape);
+            newSVG.setTextElement(answer.text, answer.textColor);
+
+            console.log(newSVG.render());
+
+            const svgData = newSVG.render();
+
+            fs.writeFile('logo.svg', svgData, function (err) {
+                if (err) throw err;
+                console.log('Logo saved to logo.svg');
+            });
+
+        });
 };
 
+
 init();
-
-
-
